601e23ea539a68175d7d5f6e18fba00f986af66d
TusharSrivastav-27 <71928146+TusharSrivastav-27@users.noreply.github.com>
2021-10-17 23:57:20 +0530

    Update Voice_Assistant.py

--- TusharSrivastav-27/Voice_Assistant.py
+++ TusharSrivastav-27/Voice_Assistant.py
@@ -42,7 +42,7 @@ while True:
 
  query=takecommand().lower()
  if 'activate' in query:
-   pyttsx3.speak("activating jarvis sir")
+   pyttsx3.speak("activating Jarvis sir")
 
    converter=pyttsx3.init()
 
