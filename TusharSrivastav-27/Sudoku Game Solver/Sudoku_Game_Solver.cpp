09bc3f1d221aee451d189fd56ed445ff03b7aeef
TusharSrivastav-27 <71928146+TusharSrivastav-27@users.noreply.github.com>
2021-10-26 21:58:35 +0530

    Update Sudoku_Game_Solver.cpp

--- TusharSrivastav-27/Sudoku Game Solver/Sudoku_Game_Solver.cpp
+++ TusharSrivastav-27/Sudoku Game Solver/Sudoku_Game_Solver.cpp
@@ -18,3 +18,25 @@ bool isSudokuSolved(int grid[9][9]){  //ckecks if sudoku is solved
     }
     return true;
 }
+void disp(int grid[9][9]){      //for displaying the whole 9x9 grid
+    for(int i=0;i<9;i++)
+    {
+        if(i==3 || i==6)
+        {
+            for(int j=0;j<66;j++)
+            {
+                cout << "-";
+            }
+            cout << endl;
+        }
+        for(int j=0;j<9;j++)
+        {
+            cout << grid[i][j] << "\t";
+            if(j==2||j==5)
+            {
+                cout << "|";
+            }
+        }
+        cout << endl;
+    }
+}
