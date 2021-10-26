aafde52fd80894c8f92e562de6756c6b80a07e60
TusharSrivastav-27 <71928146+TusharSrivastav-27@users.noreply.github.com>
2021-10-26 21:57:57 +0530

    Update Sudoku_Game_Solver.cpp

--- TusharSrivastav-27/Sudoku Game Solver/Sudoku_Game_Solver.cpp
+++ TusharSrivastav-27/Sudoku Game Solver/Sudoku_Game_Solver.cpp
@@ -4,3 +4,16 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
+bool isSudokuSolved(int grid[9][9]){  //ckecks if sudoku is solved
+    for(int i=0;i<9;i++)
+    {
+        for(int j=0;j<9;j++)
+        {
+            if(grid[i][j] > 9)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
